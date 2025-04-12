f808f07e6221dd6dc42208e3f36787939b53e967
microprixs <arpit.mishra@microprixs.in>
2025-04-12 19:39:33 +0530

    camel case to snake case program

--- loops.py
+++ loops.py
@@ -1,4 +1,25 @@
-index = 3
-while index != 0:
-    print("Meow Meow!!")
-    index -= 1
+# index = 3
+# while index != 0:
+#     print("Meow Meow!!")
+#     index -= 1
+
+
+# program to convert camel case to snake case
+
+def main(text):
+    result = change_case(text)
+    print(f"snake_case: {result}")
+
+def change_case(str):
+    new_str = ""
+    for letter in str:
+        if letter.isupper():
+            new_str += "_"
+        new_str += letter.lower()
+    return new_str
+            
+
+
+text = input("camelCase: ")
+if __name__ == "__main__":
+    main(text)
